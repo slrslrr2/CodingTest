a040321d334ceccb862cd97e05fec0bbdb272604
slrslrr2 <slrslrr1@gmail.com>
2022-03-14 22:19:42 +0900

    Stack, Queue(자료구조) - 2. 괄호문자제거

---
+++ src/main/java/five/Two.java
@@ -0,0 +1,46 @@
+package five;
+
+import java.util.Scanner;
+/**
+2. 괄호문자제거
+    설명
+    입력된 문자열에서 소괄호 ( ) 사이에 존재하는 모든 문자를 제거하고 남은 문자만 출력하는 프로그램을 작성하세요.
+
+    입력
+    첫 줄에 문자열이 주어진다. 문자열의 길이는 100을 넘지 않는다.
+
+    출력
+    남은 문자만 출력한다.
+
+    예시 입력 1
+    (A(BC)D)EF(G(H)(IJ)K)LM(N)
+
+    예시 출력 1
+    EFLM
+**/
+
+public class Two {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        solution(str);
+    }
+
+    private static void solution(String str) {
+        char[] chars = str.toCharArray();
+        String answer = "";
+        int left = 0;
+
+        for (int i=0; i<chars.length; i++) {
+            if(chars[i] == '('){
+                left++;
+            } else if(chars[i] == ')'){
+                left--;
+            } else if(left == 0){
+                answer += String.valueOf(chars[i]);
+            }
+        }
+        System.out.println(answer);
+    }
+}
